3311248cf90bb9fe4c03ec4db2e55ef4048c24fb
ukar1mov <ulugbekbekkarimov@gmail.com>
2022-11-21 00:57:01 +0500

    for loop, switch statement

---
+++ index.js
@@ -0,0 +1,109 @@
+
+
+
+// let a = 4;
+
+// if (a == 1){
+
+// }
+// if (a == 2){
+
+// }
+// if (a == 3){
+
+// }
+// if (a == 4){
+
+// }
+
+
+// console.log(0 || 1);
+// console.log(0 ?? 1);
+
+
+// ============= Switch statement
+
+//if (condition) -> ==, ===. <,>, <=,>=
+
+// switch(variable) -> ===
+
+// let data = "2";
+
+// switch (data){
+//     case "2":
+//         console.log("bugun yomgir");
+//         break;
+//     case "3": 
+//         console.log('ertaga qor');
+//         break;
+//     case "4":
+//         console.log("keca qor edi");
+//         break;
+//         default: console.log("no data found");
+// }
+
+// ================for loop
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+
+// for(boshlanish, manzil, step);
+
+
+// for(i = 0; i < 10; i++){
+//     console.log('hi ulugbek');
+// }
+
+
+// let i = 1;
+
+// for (i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for (i = 1; i > 10; i++){
+//     console.log(hey);
+// }
+
+
+
+
+// for(i = 1; i < 12; i = i + 2){
+//     console.log(i);
+// }
+
+
+// var sum = 0;
+// for (i = 0; i < 12; i++) {
+//     console.log(sum += i);
+//     if (sum == 10) break;
+    
+// }
+// var sum = 0;
+
+// for (i = 1; i < 12; i++){
+//     console.log(`Sum = ${sum} + ${i} = ${sum += i}`);
+//     if(sum == 10) break;
+// }
+// var sum = 0;
+
+// for (i = 1; i < 12; i++){
+//     console.log(i);
+//     i += i;
+// }
+
+
+// for (i = 4; i < 20; i++){
+//     console.log(i);
+//     i += i;
+// }
+
+// for (i = 1; i <= 12; i++){
+//     if(i % 2 === 0) console.log(i);
+// }
+
